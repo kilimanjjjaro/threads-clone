f9cc2d8653a54e2a68642fbec24359302a709c74
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-08-11 20:48:22 -0300

    Clean project

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,11 +1,8 @@
-import { component$, Slot, useStyles$ } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
-import type { RequestHandler } from "@builder.io/qwik-city";
+import { component$, Slot } from '@builder.io/qwik'
+import type { RequestHandler } from '@builder.io/qwik-city'
 
-import Header from "~/components/starter/header/header";
-import Footer from "~/components/starter/footer/footer";
-
-import styles from "./styles.css?inline";
+import NavBar from '~/components/header/navbar'
+import Footer from '~/components/footer/footer'
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -14,25 +11,18 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
     // Always serve a cached response by default, up to a week stale
     staleWhileRevalidate: 60 * 60 * 24 * 7,
     // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-    maxAge: 5,
-  });
-};
-
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
+    maxAge: 5
+  })
+}
 
 export default component$(() => {
-  useStyles$(styles);
   return (
     <>
-      <Header />
+      <NavBar />
       <main>
         <Slot />
       </main>
       <Footer />
     </>
-  );
-});
+  )
+})
