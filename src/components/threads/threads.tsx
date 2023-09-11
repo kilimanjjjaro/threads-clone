478965b8e4ea5027f16d124e98696529bdffedb8
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-09-11 19:49:22 -0300

    Fix bun in threads

--- src/components/threads/threads.tsx
+++ src/components/threads/threads.tsx
@@ -7,20 +7,18 @@ export default component$(() => {
 
   return (
     <div class='flex flex-col gap-3'>
-      {threads.map((thread) => {
-        return (
-          <article
-            key={thread.id}
-            class='flex flex-col gap-3 pb-3 border-b border-threads-white/[0.15]'
-          >
-            {thread.thread_items.map((item, index) => {
-              const nested = index > 0
+      {threads.map((thread) => (
+        <article
+          key={thread.id}
+          class='flex flex-col gap-3 pb-3 border-b border-threads-white/[0.15]'
+        >
+          {thread.thread_items.map((item, index) => {
+            const nested = index > 0 && thread.thread_items.length > 1
 
-              return <Thread key={item.post.id} thread={item} nested={nested} />
-            })}
-          </article>
-        )
-      })}
+            return <Thread key={item.post.id} thread={item} nested={nested} />
+          })}
+        </article>
+      ))}
     </div>
   )
 })
