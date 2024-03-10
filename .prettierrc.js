8e8f1e068c35a6868ee3154a8ab80716f60b1633
Kilimanjjjaro <hola@kilimanjjjaro.com>
2024-03-09 22:04:19 -0300

    chore: update dependencies

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+export default {
+  plugins: ["prettier-plugin-tailwindcss"],
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: "none",
+  endOfLine: "lf",
+};
